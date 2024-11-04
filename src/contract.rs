d9ee6b8503925733f39eb421c6ca413b90504813
Gjermund Garaba <gjermund@garaba.net>
2024-11-04 20:25:39 +0100

    minimal 08-wasm dummy light client

---
+++ src/contract.rs
@@ -0,0 +1,124 @@
+#[cfg(not(feature = "library"))]
+use cosmwasm_std::entry_point;
+use cosmwasm_std::{to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response};
+use ibc_core::client::types::Height;
+use ibc_core::primitives::proto::Any;
+use ibc_proto::ibc::lightclients::wasm::v1::ClientState;
+use prost::Message;
+
+use crate::error::ContractError;
+use crate::msg::{
+    CheckForMisbehaviourResult, ExecuteMsg, InstantiateMsg, QueryMsg, StatusResult, SudoMsg,
+    TimestampAtHeightResult, UpdateStateResult,
+};
+use crate::state::{consensus_db_key, HOST_CLIENT_STATE_KEY};
+
+#[entry_point]
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    let client_state = ClientState {
+        checksum: msg.checksum.into(),
+        data: msg.client_state.clone().into(),
+        latest_height: Some(ibc_proto::ibc::core::client::v1::Height {
+            revision_number: 0,
+            revision_height: 1,
+        }),
+    };
+
+    let client_state_any = Any {
+        type_url: "/ibc.lightclients.wasm.v1.ClientState".to_string(),
+        value: client_state.encode_to_vec(),
+    };
+
+    deps.storage.set(
+        HOST_CLIENT_STATE_KEY.as_bytes(),
+        client_state_any.encode_to_vec().as_slice(),
+    );
+
+    let height = Height::new(0, 1).unwrap();
+    deps.storage.set(
+        consensus_db_key(&height).as_bytes(),
+        msg.consensus_state.as_slice(),
+    );
+
+    Ok(Response::default())
+}
+
+#[entry_point]
+pub fn sudo(_deps: DepsMut, _env: Env, msg: SudoMsg) -> Result<Response, ContractError> {
+    let result = match msg {
+        SudoMsg::VerifyMembership(_) => verify_membership()?,
+        SudoMsg::VerifyNonMembership(_) => verify_non_membership()?,
+        SudoMsg::UpdateState(_) => update_state()?,
+        SudoMsg::UpdateStateOnMisbehaviour(_) => unimplemented!(),
+        SudoMsg::VerifyUpgradeAndUpdateState(_) => todo!(),
+        SudoMsg::MigrateClientStore(_) => unimplemented!(),
+    };
+
+    Ok(Response::default().set_data(result))
+}
+
+pub fn verify_membership() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&Ok::<(), ()>(()))?)
+}
+
+pub fn verify_non_membership() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&Ok::<(), ()>(()))?)
+}
+
+pub fn update_state() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&UpdateStateResult { heights: vec![] })?)
+}
+
+#[entry_point]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    unimplemented!()
+}
+
+#[entry_point]
+pub fn query(_deps: Deps, env: Env, msg: QueryMsg) -> Result<Binary, ContractError> {
+    match msg {
+        QueryMsg::VerifyClientMessage(_) => verify_client_message(),
+        QueryMsg::CheckForMisbehaviour(_) => check_for_misbehaviour(),
+        QueryMsg::TimestampAtHeight(_) => timestamp_at_height(env),
+        QueryMsg::Status(_) => status(),
+        QueryMsg::ExportMetadata(_) => export_metadata(),
+    }
+}
+
+pub fn verify_client_message() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&Ok::<(), ()>(()))?)
+}
+
+pub fn check_for_misbehaviour() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&CheckForMisbehaviourResult {
+        found_misbehaviour: false,
+    })?)
+}
+
+pub fn timestamp_at_height(env: Env) -> Result<Binary, ContractError> {
+    let now = env.block.time.seconds();
+    Ok(to_json_binary(&TimestampAtHeightResult { timestamp: now })?)
+}
+
+pub fn status() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&StatusResult {
+        status: "Active".to_string(),
+    })?)
+}
+
+pub fn export_metadata() -> Result<Binary, ContractError> {
+    Ok(to_json_binary(&Ok::<(), ()>(()))?)
+}
+
+#[cfg(test)]
+mod tests {}
---
+++ src/msg_new.rs
@@ -0,0 +1,210 @@
+//! This module defines the types used by all light client contracts for ibc-lite
+
+use cosmwasm_schema::{cw_serde, QueryResponses};
+use cosmwasm_std::Binary;
+use ibc_client_cw::types::{
+    CheckForMisbehaviourMsgRaw, ExportMetadataMsg, StatusMsg, TimestampAtHeightMsg,
+    UpdateStateMsgRaw, UpdateStateOnMisbehaviourMsgRaw, VerifyClientMessageRaw,
+    VerifyMembershipMsgRaw, VerifyNonMembershipMsgRaw, VerifyUpgradeAndUpdateStateMsgRaw,
+};
+
+use crate::ContractError;
+
+/// Instantiate message for all light client contracts in ibc-lite
+#[cw_serde]
+pub struct InstantiateMsg {
+    /// The initial client state.
+    pub client_state: Binary,
+    /// The initial consensus state.
+    pub consensus_state: Binary,
+    /// The checksum of the client state.
+    pub checksum: Binary,
+}
+
+/// Execute messages supported by all light client contracts in ibc-lite
+#[cw_serde]
+pub enum ExecuteMsg {
+    /// Update the client state
+    UpdateState(UpdateStateMsgRaw),
+    /// Update the client state on misbehaviour
+    UpdateStateOnMisbehaviour(UpdateStateOnMisbehaviourMsgRaw),
+    /// Verify upgrade and update the client state
+    VerifyUpgradeAndUpdateState(VerifyUpgradeAndUpdateStateMsgRaw),
+}
+
+/// Query messages supported by all light client contracts in ibc-lite
+#[derive(QueryResponses)]
+#[cw_serde]
+pub enum QueryMsg {
+    /// Get the status of the client
+    #[returns(query_responses::Status)]
+    Status(StatusMsg),
+    /// Export the metadata
+    #[returns(query_responses::ExportMetadata)]
+    ExportMetadata(ExportMetadataMsg),
+    /// Get the timestamp at the given height
+    #[returns(query_responses::TimestampAtHeight)]
+    TimestampAtHeight(TimestampAtHeightMsg),
+    /// Verify the client message
+    #[returns(query_responses::VerifyClientMessage)]
+    VerifyClientMessage(VerifyClientMessageRaw),
+    /// Check for misbehaviour
+    #[returns(query_responses::CheckForMisbehaviour)]
+    CheckForMisbehaviour(CheckForMisbehaviourMsgRaw),
+    /// Verify membership
+    #[returns(ibc_client_cw::types::ContractResult)]
+    VerifyMembership(VerifyMembershipMsgRaw),
+    /// Verify non-membership
+    #[returns(ibc_client_cw::types::ContractResult)]
+    VerifyNonMembership(VerifyNonMembershipMsgRaw),
+
+    /// Get the owner of the contract
+    #[returns(String)]
+    Owner {},
+}
+
+/// Contains the query responses supported by all light client contracts in ibc-lite
+pub mod query_responses {
+    use crate::ContractError;
+
+    use super::cw_serde;
+    use ibc_client_cw::types::GenesisMetadata;
+
+    /// The response to [`super::QueryMsg::Status`]
+    #[cw_serde]
+    pub struct Status {
+        /// The status of the client
+        // TODO: Turn this into an enum
+        pub status: String,
+    }
+    /// The response to [`super::QueryMsg::ExportMetadata`]
+    #[cw_serde]
+    pub struct ExportMetadata {
+        /// The genesis metadata
+        pub metadata: Vec<GenesisMetadata>,
+    }
+    /// The response to [`super::QueryMsg::TimestampAtHeight`]
+    #[cw_serde]
+    pub struct TimestampAtHeight {
+        /// The timestamp at the given height
+        pub timestamp: u64,
+    }
+    /// The response to [`super::QueryMsg::VerifyClientMessage`]
+    #[cw_serde]
+    pub struct VerifyClientMessage {
+        /// Whether the client message is valid
+        pub is_valid: bool,
+    }
+    /// The response to [`super::QueryMsg::CheckForMisbehaviour`]
+    #[cw_serde]
+    pub struct CheckForMisbehaviour {
+        /// Whether misbehaviour was found
+        pub found_misbehaviour: bool,
+    }
+
+    impl TryFrom<ibc_client_cw::types::QueryResponse> for Status {
+        type Error = ContractError;
+
+        fn try_from(
+            query_response: ibc_client_cw::types::QueryResponse,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self {
+                status: query_response.status.unwrap(),
+            })
+        }
+    }
+
+    impl TryFrom<ibc_client_cw::types::QueryResponse> for ExportMetadata {
+        type Error = ContractError;
+
+        fn try_from(
+            query_response: ibc_client_cw::types::QueryResponse,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self {
+                metadata: query_response.genesis_metadata.unwrap(),
+            })
+        }
+    }
+
+    impl TryFrom<ibc_client_cw::types::QueryResponse> for TimestampAtHeight {
+        type Error = ContractError;
+
+        fn try_from(
+            query_response: ibc_client_cw::types::QueryResponse,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self {
+                timestamp: query_response.timestamp.unwrap(),
+            })
+        }
+    }
+
+    impl TryFrom<ibc_client_cw::types::QueryResponse> for VerifyClientMessage {
+        type Error = ContractError;
+
+        fn try_from(
+            query_response: ibc_client_cw::types::QueryResponse,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self { is_valid: true })
+        }
+    }
+
+    impl TryFrom<ibc_client_cw::types::QueryResponse> for CheckForMisbehaviour {
+        type Error = ContractError;
+
+        fn try_from(
+            query_response: ibc_client_cw::types::QueryResponse,
+        ) -> Result<Self, Self::Error> {
+            Ok(Self {
+                found_misbehaviour: query_response.found_misbehaviour.unwrap(),
+            })
+        }
+    }
+}
+
+impl From<InstantiateMsg> for ibc_client_cw::types::InstantiateMsg {
+    fn from(instantiate_msg: InstantiateMsg) -> Self {
+        Self {
+            client_state: instantiate_msg.client_state.into(),
+            consensus_state: instantiate_msg.consensus_state.into(),
+            // TODO: Add checksum
+            checksum: Vec::new(),
+        }
+    }
+}
+
+impl From<ExecuteMsg> for ibc_client_cw::types::SudoMsg {
+    fn from(execute_msg: ExecuteMsg) -> Self {
+        match execute_msg {
+            ExecuteMsg::UpdateState(msg) => Self::UpdateState(msg),
+            ExecuteMsg::UpdateStateOnMisbehaviour(msg) => Self::UpdateStateOnMisbehaviour(msg),
+            ExecuteMsg::VerifyUpgradeAndUpdateState(msg) => Self::VerifyUpgradeAndUpdateState(msg),
+        }
+    }
+}
+
+impl TryFrom<QueryMsg> for ibc_client_cw::types::QueryMsg {
+    type Error = ContractError;
+
+    fn try_from(query_msg: QueryMsg) -> Result<Self, Self::Error> {
+        match query_msg {
+            QueryMsg::Status(msg) => Ok(Self::Status(msg)),
+            QueryMsg::ExportMetadata(msg) => Ok(Self::ExportMetadata(msg)),
+            QueryMsg::TimestampAtHeight(msg) => Ok(Self::TimestampAtHeight(msg)),
+            QueryMsg::VerifyClientMessage(msg) => Ok(Self::VerifyClientMessage(msg)),
+            QueryMsg::CheckForMisbehaviour(msg) => Ok(Self::CheckForMisbehaviour(msg)),
+            _ => panic!("Unsupported query message"),
+        }
+    }
+}
+
+impl TryFrom<QueryMsg> for ibc_client_cw::types::SudoMsg {
+    type Error = ContractError;
+
+    fn try_from(query_msg: QueryMsg) -> Result<Self, Self::Error> {
+        match query_msg {
+            QueryMsg::VerifyMembership(msg) => Ok(Self::VerifyMembership(msg)),
+            QueryMsg::VerifyNonMembership(msg) => Ok(Self::VerifyNonMembership(msg)),
+            _ => panic!("Unsupported query message"),
+        }
+    }
+}
